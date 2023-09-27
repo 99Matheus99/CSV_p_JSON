bb475805e9d9d1579b000ef8432db670ae5b75b2
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-27 11:07:04 -0300

    adicionando elementos visuais

--- Conversor_CSV_JSON.py
+++ Conversor_CSV_JSON.py
@@ -3,9 +3,11 @@ chaves = ['Nome','Idade','Cidade','Email','CPF']
 dicionario_itens = copia = {}
 lista_itens = []
 with open('dados.csv', mode ='r', encoding='utf-8') as arquivo:
+    print(f'arquivo {arquivo.name} encontrado....')
     leitor_csv = csv.reader(arquivo)
     next(leitor_csv) # aqui ele não pega o cabeçalho
     items = list(leitor_csv) 
+    print(f'Processando {len(items)} registros...')
     # o objetivo é literalmente criar uma lista de dicionários  
     for linhas in items: 
         for c in range(0, len(linhas)):
@@ -14,4 +16,3 @@ with open('dados.csv', mode ='r', encoding='utf-8') as arquivo:
         # isso ocorre pois, se eu alterar um valor do dicionário, mesmo já dentro da lista, ele altera todos os valores
         copia = dict(dicionario_itens) 
         lista_itens.append(copia)       
-print(lista_itens)
