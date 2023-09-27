7f8daca09e901f044b4af876cf24d08757ad406c
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-27 11:20:07 -0300

    adicionando a abertura a atualição do arquivo json

--- Conversor_CSV_JSON.py
+++ Conversor_CSV_JSON.py
@@ -1,9 +1,10 @@
 import csv
+import json
 chaves = ['Nome','Idade','Cidade','Email','CPF']
 dicionario_itens = copia = {}
 lista_itens = []
 with open('dados.csv', mode ='r', encoding='utf-8') as arquivo:
-    print(f'arquivo {arquivo.name} encontrado....')
+    print(f'arquivo {arquivo.name} encontrado...')
     leitor_csv = csv.reader(arquivo)
     next(leitor_csv) # aqui ele não pega o cabeçalho
     items = list(leitor_csv) 
@@ -14,5 +15,8 @@ with open('dados.csv', mode ='r', encoding='utf-8') as arquivo:
             dicionario_itens[chaves[c]] = linhas[c]
         # aqui eu criei essa cópia do dicionário
         # isso ocorre pois, se eu alterar um valor do dicionário, mesmo já dentro da lista, ele altera todos os valores
-        copia = dict(dicionario_itens) 
-        lista_itens.append(copia)       
+        copia = dict(dicionario_itens)
+        lista_itens.append(copia)
+with open('dados.json', mode ='w', encoding='utf-8') as arquivo_json:
+    print(f'arquivo {arquivo_json.name} gerado com sucesso!')
+    json.dump(lista_itens, arquivo_json, indent=2, ensure_ascii=False)
